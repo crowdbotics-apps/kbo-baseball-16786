432b432b5282e907b743d49ad49400fb217bbe99
Crowdbotics <team@crowdbotics.com>
2020-05-09 04:55:49 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/kbo_baseball_16786/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kbo_baseball_16786;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "kbo_baseball_16786";
+    }
+}
